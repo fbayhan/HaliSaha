f91d8d31965e7a873cd777137f44feaa296ba2cb
fbayhan <fatih-bayhan@windowslive.com>
2023-05-04 21:11:45 +0300

    material ui added, simple player list implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [players, setPlayers] = useState([]);
+    const [playerName, setPlayerName] = useState("");
+
+    function sayHello() {
+        setCount(count + 1)
+    }
+
+    function handleChange() {
+        setPlayerName(playerName);
+        setCount(count + 1)
+
+        setPlayers(current => [...current, playerName])
+
+    }
+
+    return (
+        <div >
+            <p>You clicked {count} times  {playerName}</p>
+
+            <Box component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
+                noValidate autoComplete="off">
+                <TextField id="outlined-basic" label="Outlined" variant="outlined"   onChange={e => setPlayerName(e.target.value)} />
+                <Button variant="contained" onClick={handleChange}>EKLE</Button>
+
+                <ul>
+
+                    {players.map((player, index) => (
+                        <li key={index}>{player}</li>
+                    ))}
+
+                </ul>
+            </Box>
+
+
+
+        </div>
+    );
 }
 
 export default App;
