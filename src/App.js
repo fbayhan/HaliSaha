7f055929f84a53edebf00a439ef24f9ed34948b3
fbayhan <fatih-bayhan@windowslive.com>
2023-05-04 21:18:19 +0300

    simple changes with player names

--- src/App.js
+++ src/App.js
@@ -4,33 +4,34 @@ import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+
 function App() {
     const [count, setCount] = useState(0);
     const [players, setPlayers] = useState([]);
     const [playerName, setPlayerName] = useState("");
 
-    function sayHello() {
-        setCount(count + 1)
-    }
 
     function handleChange() {
         setPlayerName(playerName);
-        setCount(count + 1)
-
-        setPlayers(current => [...current, playerName])
-
+        if (playerName != "")
+            setPlayers(current => [...current, playerName]);
     }
 
     return (
-        <div >
-            <p>You clicked {count} times  {playerName}</p>
+        <Container fixed>
+
+            <h1>Room 5004</h1>    <h2>29 Mayıs 2022 Atatürk Halı Lisesi</h2>
+            <p>You added {playerName}</p>
 
             <Box component="form"
-                sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
-                noValidate autoComplete="off">
-                <TextField id="outlined-basic" label="Outlined" variant="outlined"   onChange={e => setPlayerName(e.target.value)} />
+                 sx={{'& > :not(style)': {m: 1, width: '25ch'},}}
+                 noValidate autoComplete="off">
+                <TextField id="outlined-basic" label="Outlined"
+                           onChange={e => setPlayerName(e.target.value)}/>
                 <Button variant="contained" onClick={handleChange}>EKLE</Button>
-
+                <div>{players.length} player added</div>
                 <ul>
 
                     {players.map((player, index) => (
@@ -41,8 +42,7 @@ function App() {
             </Box>
 
 
-
-        </div>
+        </Container>
     );
 }
 
