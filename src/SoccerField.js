c68804fc5d740cc771c94538ce1bbf62e7c2fdbe
fbayhan <fatih-bayhan@windowslive.com>
2023-05-15 22:02:27 +0300

    Team stats building

--- src/SoccerField.js
+++ src/SoccerField.js
@@ -5,6 +5,12 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import {useEffect, useLayoutEffect, useRef, useState} from 'react';
 
+import SoccerFieldImage from './images/SoccerField.png'
+import IconButton from '@mui/material/IconButton';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+
 function SoccerField(props) {
 
     const ref = useRef(null);
@@ -24,39 +30,101 @@ function SoccerField(props) {
         textAlign: 'center',
         color: theme.palette.text.secondary,
     }));
+    function onDragEnd(result) {
+        console.log("onDragEnd")
+        console.log("onDragEnd")
+    }
 
     return (
-        <div style={{ backgroundColor:"red"}}>
+        <div>
             <h2>Player Wizard</h2>
+            <hr/>
             <Box sx={{flexGrow: 1}}>
                 <Grid container spacing={2}>
-                    <Grid item xs={8} >
-                        Saha
-                        <Item style={{ backgroundColor:"blue",  display: 'flex',
+
+                    <DragDropContext onDragEnd={onDragEnd}>
+
+                    <Grid item xs={8}>
+                        <h3>Saha</h3>
+                        <Item style={{
+                            display: 'flex',
                             alignItems: 'center',
-                            justifyContent: 'center',}}>xs=8
-                            <div ref={ref} style={{
-                                color: "white",display: 'flex',
-                                backgroundColor: "green", width: '80%', height: width * 1.6,  alignItems: 'center',
-                                justifyContent: 'center'
-                            }}>
-                                <div style={{ border: '2px solid white' , width:'90%', height:'95%'}}>
-
-                                    <div style={{ border: '2px solid white', width: '25%', height:'10%', ali }}>Kale 1</div>
-                                    <div>Orta Çizgi
-                                        <div>Yuvarlak</div>
-                                    </div>
-
-                                    <div>Kale 2</div>
-                                </div>
+                            justifyContent: 'center',
+                        }}>
+                            <div ref={ref}>
+                                <img src={SoccerFieldImage} width="100%" height="100%"/>
                             </div>
 
                         </Item>
                     </Grid>
+
+                        <Droppable droppableId="firstTeam">
                     <Grid item xs={4}>
-                        Oyuncular
-                        <Item>xs=4</Item>
+                        <h3> Oyuncular</h3>
+
+
+                        <ul className="characters">
+                            <Draggable>
+                            <li>
+                                <span>1</span>
+                                <p>Fatih Bayhan </p>
+                                <IconButton color="primary"
+                                            aria-label="upload picture"
+                                            component="label">
+
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </li>
+                            </Draggable>
+                            <li>
+                                <span>3</span>
+                                <p>Fatih Bayhan </p>
+                                <IconButton color="primary"
+                                            aria-label="upload picture"
+                                            component="label">
+
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </li>
+                            <li>
+                                <span>5</span>
+                                <p>Fatih Bayhan </p>
+                                <IconButton color="primary"
+                                            aria-label="upload picture"
+                                            component="label">
+
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </li>
+                            <li>
+                                <span>6</span>
+                                <p>Fatih Bayhan </p>
+                                <IconButton color="primary"
+                                            aria-label="upload picture"
+                                            component="label">
+
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </li>
+                            <li>
+                                <span>7</span>
+                                <p>Fatih Bayhan </p>
+                                <IconButton color="primary"
+                                            aria-label="upload picture"
+                                            component="label">
+
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </li>
+
+
+                        </ul>
+
+
                     </Grid>
+                        </Droppable>
+
+                    </DragDropContext>
                 </Grid>
             </Box>
 
