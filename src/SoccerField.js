c3b805127217d3554910b6f101d5e47e3660d2a1
fbayhan <fatih-bayhan@windowslive.com>
2023-05-20 01:51:18 +0300

    pushed with ref problem

--- src/SoccerField.js
+++ src/SoccerField.js
@@ -11,18 +11,21 @@ import SoccerFieldImage from './images/SoccerField.png'
 import IconButton from '@mui/material/IconButton';
 
 import DeleteIcon from '@mui/icons-material/Delete';
+import SettingsAccessibilityIcon from '@mui/icons-material/SettingsAccessibility';
+
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 
 function SoccerField(props) {
 
-    const ref = useRef(null);
+
+    const soccerField = useRef(null);
 
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
 
     useLayoutEffect(() => {
-        setWidth(ref.current.clientWidth);
-        setHeight(ref.current.clientHeight);
+        setWidth(soccerField.current.clientWidth);
+        setHeight(soccerField.current.clientHeight);
     }, []);
 
     const Item = styled(Paper)(({theme}) => ({
@@ -42,17 +45,160 @@ function SoccerField(props) {
         soccerField: {
             width: "100%",
             aspectRatio: 0.67,
-            color: "blue",
             backgroundImage: `url('${SoccerFieldImage}')`,
             //   ` ve ' arasındaki farka bak
             backgroundPosition: 'center',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
+            position: "relative"
+        },
+        fieldPlayer: {
+            display: 'block',
+            border: "solid 2px   #000000",
+            borderRadius: "50%",
+            color: "red",
+            aspectRatio: 1,
+            width: width * 0.1,
+            fontSize: width * 0.08,
+            position: "absolute",
 
 
         }
-
     }
+
+
+    const firstTeam = [
+        {
+            "id": 1,
+            "GoalKeeper": false,
+            "Name": "Fatih Bayhan",
+            "BackNumber": 38,
+            "top": 125,
+            "left": 150
+        },
+        {
+            "id": 2,
+            "GoalKeeper": false,
+            "Name": "Fatih Bayhan",
+            "BackNumber": 2,
+            "top": 125,
+            "left": 350
+        },
+        {
+            "id": 3,
+            "GoalKeeper": false,
+            "Name": "Nurcan",
+            "BackNumber": 3,
+            "top": 125,
+            "left": 550
+        },
+        {
+            "id": 4,
+            "GoalKeeper": false,
+            "Name": "Leyla",
+            "BackNumber": 4,
+            "top": 225,
+            "left": 450
+        },
+        {
+            "id": 5,
+            "GoalKeeper": false,
+            "Name": "Ali",
+            "BackNumber": 5,
+            "top": 325,
+            "left": 150
+        },
+        {
+            "id": 6,
+            "GoalKeeper": false,
+            "Name": "Hakan",
+            "BackNumber": 12,
+            "top": 325,
+            "left": 250
+        },
+        {
+            "id": 7,
+            "GoalKeeper": false,
+            "Name": "Alperen",
+            "BackNumber": 7,
+            "top": 325,
+            "left": 450
+        },
+        {
+            "id": 8,
+            "GoalKeeper": false,
+            "Name": "Marla",
+            "BackNumber": 8,
+            "top": 425,
+            "left": 250
+        },
+        {
+            "id": 9,
+            "GoalKeeper": false,
+            "Name": "Fatih Parlak",
+            "BackNumber": 9,
+            "top": 425,
+            "left": 350
+        }
+    ];
+
+    const secondTeam = [
+        {
+            "id": 10,
+            "GoalKeeper": false,
+            "Name": "Alfa",
+            "BackNumber": 1
+        },
+        {
+            "id": 11,
+            "GoalKeeper": false,
+            "Name": "Beta",
+            "BackNumber": 2
+        },
+        {
+            "id": 12,
+            "GoalKeeper": false,
+            "Name": "Utku",
+            "BackNumber": 3
+        },
+        {
+            "id": 13,
+            "GoalKeeper": false,
+            "Name": "Batu",
+            "BackNumber": 4
+        },
+        {
+            "id": 14,
+            "GoalKeeper": false,
+            "Name": "Ece",
+            "BackNumber": 5
+        },
+        {
+            "id": 15,
+            "GoalKeeper": false,
+            "Name": "Nurcan",
+            "BackNumber": 6
+        },
+        {
+            "id": 16,
+            "GoalKeeper": false,
+            "Name": "Bilge",
+            "BackNumber": 7
+        },
+        {
+            "id": 17,
+            "GoalKeeper": false,
+            "Name": "Begüm",
+            "BackNumber": 8
+        },
+        {
+            "id": 18,
+            "GoalKeeper": false,
+            "Name": "Turan Bayhan",
+            "BackNumber": 9
+        }
+    ];
+
 
     return (
         <div>
@@ -60,20 +206,32 @@ function SoccerField(props) {
             <hr/>
             <Box sx={{flexGrow: 1}}>
                 <Grid container spacing={2}>
-
                     <DragDropContext onDragEnd={onDragEnd}>
 
                         <Grid item xs={8}>
                             <h3>Saha</h3>
-                            <Item style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                            }}>
-                                <div ref={ref} style={styles.soccerField}>
-                                      {/*<img src={SoccerFieldImage} width="100%" height="100%"/>*/}
-                                </div>
+                            <Item style={{}}>
+                                <Droppable droppableId="field">
+                                    {(provided, snapshot) => (
+                                        <div ref={(el)=> {soccerField(el); provided.innerRef(el);}} ref={soccerField} style={styles.soccerField}  {...provided.droppableProps} >
 
+                                            {firstTeam.map(({id, GoalKeeper, Name, BackNumber, top, left}, index) => {
+                                                return (
+                                                    <Draggable key={id} draggableId={id + ""} index={index}>
+                                                        {(provided) => (
+                                                    <span style={{
+                                                        ...styles.fieldPlayer, left: left,
+                                                        top: top
+                                                    }}  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >{BackNumber}</span>
+                                                        )}
+                                                    </Draggable>
+                                                )
+                                            })}
+
+
+                                        </div>
+                                    )}
+                                </Droppable>
                             </Item>
                         </Grid>
 
@@ -81,60 +239,61 @@ function SoccerField(props) {
                         <Grid item xs={4}>
                             <h3> Oyuncular</h3>
 
+                            <h4>Takım 1</h4>
+
+                            <Droppable droppableId="team1">
+                                {(provided, snapshot) => (
+                                    <ul className="characters"  {...provided.droppableProps} ref={provided.innerRef}>
+                                        {firstTeam.map(({id, GoalKeeper, Name, BackNumber}, index) => {
+                                            return (
+                                                <Draggable key={id} draggableId={id + ""} index={index}>
+                                                    {(provided) => (
+                                                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                            <span>{BackNumber}</span>
+                                                            <p>{Name} </p>
+                                                            <IconButton color="primary"
+                                                                        aria-label="upload picture"
+                                                                        component="label">
+                                                                <DeleteIcon/>
+                                                            </IconButton>
+                                                        </li>
+                                                    )}
+                                                </Draggable>
+                                            );
+                                        })}
+                                    </ul>
+                                )}
+                            </Droppable>
+                            <br/>
+                            <br/>
+                            <h4>Takım 2</h4>
 
                             <ul className="characters">
 
-                                <li>
-                                    <span>1</span>
-                                    <p>Fatih Bayhan </p>
-                                    <IconButton color="primary"
-                                                aria-label="upload picture"
-                                                component="label">
-
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </li>
-
-                                <li>
-                                    <span>3</span>
-                                    <p>Fatih Bayhan </p>
-                                    <IconButton color="primary"
-                                                aria-label="upload picture"
-                                                component="label">
-
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </li>
-                                <li>
-                                    <span>5</span>
-                                    <p>Fatih Bayhan </p>
-                                    <IconButton color="primary"
-                                                aria-label="upload picture"
-                                                component="label">
-
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </li>
-                                <li>
-                                    <span>6</span>
-                                    <p>Fatih Bayhan </p>
-                                    <IconButton color="primary"
-                                                aria-label="upload picture"
-                                                component="label">
-
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </li>
-                                <li>
-                                    <span>7</span>
-                                    <p>Fatih Bayhan </p>
-                                    <IconButton color="primary"
-                                                aria-label="upload picture"
-                                                component="label">
-
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </li>
+                                <Droppable droppableId="team2">
+                                    {(provided, snapshot) => (
+                                        <ul className="characters"  {...provided.droppableProps}
+                                            ref={provided.innerRef}>
+                                            {secondTeam.map(({id, GoalKeeper, Name, BackNumber}, index) => {
+                                                return (
+                                                    <Draggable key={id} draggableId={id + ""} index={index}>
+                                                        {(provided) => (
+                                                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                                <span>{BackNumber}</span>
+                                                                <p>{Name} </p>
+                                                                <IconButton color="primary"
+                                                                            aria-label="upload picture"
+                                                                            component="label">
+                                                                    <DeleteIcon/>
+                                                                </IconButton>
+                                                            </li>
+                                                        )}
+                                                    </Draggable>
+                                                );
+                                            })}
+                                        </ul>
+                                    )}
+                                </Droppable>
 
 
                             </ul>
